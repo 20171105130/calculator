04c63a2c2275cc0047fce1f45b87fda0fe2380eb
20171105130 <1198746397@qq.com>
2018-09-27 08:49:21 +0800

    计算器
    第一步加控件

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var rulst: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func number1(_ sender: Any) {}
+    @IBAction func number2(_ sender: Any) {}
+    @IBAction func number3(_ sender: Any) {}
+    @IBAction func number4(_ sender: Any) {}
+    @IBAction func number5(_ sender: Any) {}
+    @IBAction func number6(_ sender: Any) {}
+    @IBAction func number7(_ sender: Any) {}
+    @IBAction func number8(_ sender: Any) {}
+    @IBAction func number9(_ sender: Any) {}
+    @IBAction func number0(_ sender: Any) {}
+    @IBAction func add(_ sender: Any) {}
+    @IBAction func less(_ sender: Any) {
+    }
+    @IBAction func multiply(_ sender: Any) {
+    }
+    @IBAction func except(_ sender: Any) {
+    }
+    
 }
+
 
